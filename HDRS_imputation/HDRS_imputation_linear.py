318ec910dfa1deee04a29222b6d4c4f14580c1f2
Asma Ghandeharioun <asma_gh@mit.edu>
2017-04-30 17:00:42 -0400

    regression survey imputation finalized

--- HDRS_imputation/HDRS_imputation_linear.py
+++ HDRS_imputation/HDRS_imputation_linear.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from my_constants import *
 
 def plot_imputation(df, df_imputed):
@@ -10,16 +11,18 @@ def plot_imputation(df, df_imputed):
     plt.legend(loc=2, scatterpoints=1)
     plt.show()
 
+
 def impute_linear():
     HAMD = pd.read_csv(data_dir+'daily_survey_HAMD.csv')
     df = HAMD[HAMD['group']=='MDD']
     df = df.reset_index(drop=True)
+    df = df.apply(is_imputed, axis=1)
 
     df_imputed = df[['HAMD']].interpolate()
 
-    df['HAMD']=df_imputed['HAMD']
+    df['HAMD'] = np.round(df_imputed['HAMD'])
 
-    df[['date', 'ID', 'HAMD']].to_csv(data_dir+'HAMD_imputed_linear.csv', index=False)
+    df[['date', 'ID', 'HAMD', 'imputed']].to_csv(data_dir+'HAMD_imputed_linear.csv', index=False)
 
 
 
--- HDRS_imputation/HDRS_imputation_survey.py
+++ HDRS_imputation/HDRS_imputation_survey.py
@@ -19,32 +19,32 @@ BEST_MDL_NAME = None
 BEST_MDL = None
 
 
-def split_data_ind(inds):
-    #TODO: not the first score, no two from the same person
-    test_N = int(len(inds)*0.1)
+def fill_nan(series):
+    series['original'] = True
+    if np.isnan(series['total_PA']):
+        series['original'] = False
+        series['total_PA'] = series['avg_overall_PA']
+        series['total_NA'] = series['avg_overall_NA']
+        series['total_NA/PA'] = series['total_NA']/series['total_PA']
+    if np.isnan(series['avg_weekly_PA']):
+        series['original'] = False
+        series['avg_weekly_PA'] = series['avg_overall_PA']
+        series['avg_weekly_NA'] = series['avg_overall_NA']
+        series['avg_weekly_NA/PA'] = series['avg_overall_PA']/series['avg_overall_NA']
+        series['weighted_avg_weekly_PA'] = series['avg_overall_PA']
+        series['weighted_avg_weekly_NA'] = series['avg_overall_NA']
+        series['weighted_avg_weekly_NA/PA'] = series['avg_overall_PA']/series['avg_overall_NA']
+        series['std_weekly_PA'] = series['std_overall_PA']
+        series['std_weekly_NA'] = series['std_overall_NA']
+    return series
+
+
+def split_data_ind(inds, test_N):
     np.random.shuffle(inds)
     ind_train = inds[test_N:]
     ind_test = inds[0:test_N]
     return ind_train, ind_test
 
-def split_data_old(x, y):
-    #not the first score, no two from the same person
-    test_N = int(len(y)*0.2)
-    train_N = len(y) - test_N
-    ind_train = list(np.arange(len(y)))
-    ind_test = [2, 5, 10, 17, 25, 28]
-#     ind_test = [2]
-    for ind in ind_test:
-        ind_train.remove(ind)
-#     inds = np.arange(len(y))
-#     np.random.shuffle(inds)
-#     ind_train = inds[test_N:]
-#     ind_test = inds[0:test_N]
-    x_train = x[ind_train]
-    y_train = y[ind_train]
-    x_test = x[ind_test]
-    y_test = y[ind_test]
-    return ind_train, x_train, y_train, ind_test, x_test, y_test
 
 def loo_split_data(x):
     loo = LeaveOneOut()
@@ -52,7 +52,8 @@ def loo_split_data(x):
     for train, test in splits:
         print("%s %s" % (train, test))
 
-def impute(inp_x, inp_y, ttl, mdl, ind_train, ind_test):
+
+def impute(inp_x, inp_y, ttl, mdl, ind_train, ind_test, model_file):
 
     global BEST_VALIDATION_RMSE, BEST_X, BEST_Y, BEST_TTL, BEST_MDL_NAME, BEST_MDL
 
@@ -71,9 +72,9 @@ def impute(inp_x, inp_y, ttl, mdl, ind_train, ind_test):
     elif mdl == 'theil':
         regr = linear_model.TheilSenRegressor(random_state=SEED)
     elif mdl == 'ransac':
-        regr = linear_model.RANSACRegressor(random_state=SEED)
+        regr = linear_model.RANSACRegressor(random_state=SEED, min_samples=0.2)
     elif mdl == 'huber':
-        regr = linear_model.HuberRegressor(random_state=SEED)
+        regr = linear_model.HuberRegressor(epsilon=2.0)
 
 
     inds = range(len(y))
@@ -92,8 +93,8 @@ def impute(inp_x, inp_y, ttl, mdl, ind_train, ind_test):
         # Train the model using the training sets
         regr.fit(x_train, y_train)
 
-        validation_RMSE = np.sqrt(mean_squared_error(y_validation, regr.predict(x_validation)))
-        train_RMSE = np.sqrt(mean_squared_error(y_train, regr.predict(x_train)))
+        validation_RMSE = np.sqrt(mean_squared_error(y_validation, np.round(regr.predict(x_validation))))
+        train_RMSE = np.sqrt(mean_squared_error(y_train, np.round(regr.predict(x_train))))
 
         avg_train_RMSE += train_RMSE
         avg_validation_RMSE += validation_RMSE
@@ -110,101 +111,74 @@ def impute(inp_x, inp_y, ttl, mdl, ind_train, ind_test):
         BEST_VALIDATION_RMSE = avg_validation_RMSE
 
     print mdl+', '+ttl+', train RMSE: %f, validation RMSE: %f ' %(avg_train_RMSE, avg_validation_RMSE)
-
-def legacy_impute_without_first_inds(x, y, ttl):
-    global first_inds
-
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-
-    inds = range(len(x))
-    for i in first_inds:
-        inds.remove(i)
-    loo = LeaveOneOut()
-    splits = loo.split(inds)
-    avg_train_RMSE = 0
-    avg_test_RMSE = 0
-    for tmp_train_inds, test_inds in splits:
-        train_inds = list(tmp_train_inds) + list(first_inds)
-        x_train = x[train_inds]
-        y_train = y[train_inds]
-        x_test = x[test_inds]
-        y_test = y[test_inds]
-
-        # Train the model using the training sets
-        regr.fit(x_train, y_train)
-
-        test_RMSE = np.sqrt(mean_squared_error(y_test, regr.predict(x_test)))
-        train_RMSE = np.sqrt(mean_squared_error(y_train, regr.predict(x_train)))
-
-        avg_train_RMSE += train_RMSE
-        avg_test_RMSE += test_RMSE
-
-    avg_train_RMSE /= len(inds)
-    avg_test_RMSE /= len(inds)
-
-    print ttl+' ,train RMSE:%f, test RMSE: %f ' %(avg_train_RMSE, avg_test_RMSE)
-
-
-def legacy_impute(x, y, ttl):
-    ind_train, x_train, y_train, ind_test, x_test, y_test = split_data(x, y)
-
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-
-    # Train the model using the training sets
-    regr.fit(x_train, y_train)
-
-    # print sklearn.feature_selection.f_regression(x, y, center=True)
-    print regr.coef_
-    print
-
-    # err = 1.0-5.0/4.0*(1-r2_score(y_test, regr.predict(x_test), multioutput='variance_weighted'))
-
-    test_RMSE = np.sqrt(mean_squared_error(y_test, regr.predict(x_test)))
-    train_RMSE = np.sqrt(mean_squared_error(y_train, regr.predict(x_train)))
-    print test_RMSE
-
-
-    plt.figure(figsize=(16,4))
-
-    plt.scatter(range(len(y)), y, label='HDRS', color = 'green', alpha=0.5)
-    plt.scatter(ind_train, regr.predict(x_train), label='predicted HDRS - train', color = 'red', alpha=0.5)
-    plt.scatter(ind_test, regr.predict(x_test), label='predicted HDRS - test', color = 'black', alpha=0.5)
-
-    plt.title(ttl + ', train RMSE: '+'{:.3f}'.format(train_RMSE)+', test RMSE: '+'{:.3f}'.format(test_RMSE))
-    plt.ylabel('HDRS')
-
-    plt.legend(loc=2, scatterpoints=1)
+    model_file.write(mdl+', '+ttl+', train RMSE: %f, validation RMSE: %f \n' %(avg_train_RMSE, avg_validation_RMSE))
 
 
 def plot_prediction(x, y, ttl, mdl_name, mdl, validation_RMSE, ind_train, ind_test):
 
-    test_RMSE = np.sqrt(mean_squared_error(np.array(y)[ind_test], mdl.predict(np.array(x)[ind_test])))
+    test_RMSE = np.sqrt(mean_squared_error(np.array(y)[ind_test], np.round(mdl.predict(np.array(x)[ind_test]))))
 
     plt.figure(figsize=(16, 4))
     plt.scatter(range(len(y)), y, label='HDRS', color='green', alpha=0.5)
-    plt.scatter(ind_train, mdl.predict(np.array(x)[ind_train]), label='predicted HDRS - train', color='red', alpha=0.5)
-    plt.scatter(ind_test, mdl.predict(np.array(x)[ind_test]), label='predicted HDRS - test', color='black', alpha=0.5)
+    plt.scatter(ind_train, np.round(mdl.predict(np.array(x)[ind_train])), label='predicted - train', color='red', alpha=0.5)
+    plt.scatter(ind_test, np.round(mdl.predict(np.array(x)[ind_test])), label='predicted - test', color='black', alpha=0.5)
     plt.title('model: '+mdl_name+', dataset: '+ttl +
               ', validation RMSE: '+'{:.3f}'.format(validation_RMSE)+
               ', test RMSE: '+'{:.3f}'.format(test_RMSE))
     plt.ylabel('HDRS')
     plt.legend(loc=2, scatterpoints=1)
 
-    print 'model parameters: \n'
-    print mdl.coef_
+    model_file = open(MODEL_FILE, "a+")
+    model_file.write('\nBest Model: '+mdl_name+', '+ttl+', validation RMSE: %f, test RMSE: %f \n' %(validation_RMSE, test_RMSE))
+    if mdl != 'ransac':
+        print 'model parameters: \n'
+        print mdl.coef_
+        model_file.write('coefficients:\n')
+        for item in mdl.coef_:
+            model_file.write('%s \t' % item)
+        model_file.write('\n')
+    model_file.close()
 
     plt.show()
 
+
+def impute_using_best_model(df, ttl, mdl_name, mdl):
+    if ttl != 'PANAS':
+        print 'NOT PANAS'
+        return
+    df = df.apply(is_imputed, axis=1)
+    def fill_HAMD(series):
+        x = series[['avg_weekly_PA','avg_weekly_NA','avg_weekly_NA/PA',
+                    'weighted_avg_weekly_PA','weighted_avg_weekly_NA','weighted_avg_weekly_NA/PA',
+                    'total_PA','total_NA','total_NA/PA',
+                    'avg_overall_PA', 'avg_overall_NA','avg_overall_NA/PA',
+                    'std_weekly_PA', 'std_weekly_NA',
+                    'std_overall_PA', 'std_overall_NA']]
+        y = series['HAMD']
+        if np.isnan(y):
+            series['HAMD'] = np.round(mdl.predict(x.reshape(1, -1))).flatten()[0]
+        return series
+    df = df.apply(fill_HAMD, axis=1)
+
+    df[['date', 'ID', 'HAMD', 'imputed']].to_csv(data_dir+'HAMD_imputed_survey.csv', index=False)
+
+    return
+
 all_df, x_df, y_df = preprocess_survey_x_y()
 
+
 subset_df = all_df.dropna(subset=['HAMD'])
+subset_df = subset_df[subset_df['ID']!='M005']
 subset_df = subset_df[subset_df['day']>=0].reset_index(True)
 first_inds = subset_df[subset_df['day']<14].index
-x_df = subset_df.drop(['ID','HAMD'], inplace=False, axis=1)
+x_df = subset_df.drop(['ID','HAMD', 'date'], inplace=False, axis=1)
 y_df = subset_df[['ID', 'HAMD']]
-x_df_nonan = x_df.fillna(0)
+x_df_nonan = x_df
+x_df_nonan = x_df_nonan.apply(fill_nan, axis=1)
+non_original_inds = x_df_nonan[x_df_nonan['original']==False].index
+x_df_nonan.drop(['original'], inplace=True, axis=1)
+x_df_nonan = x_df_nonan.fillna(0)
+
 reduced_x_df, reduced_n = reduce_dimensionality(x_df_nonan, max_n=30, threshold=EXPLAINED_VARIANCE_THRESHOLD)
 
 PANAS_df = x_df_nonan[['avg_weekly_PA','avg_weekly_NA','avg_weekly_NA/PA',
@@ -225,6 +199,7 @@ PANAS_short_weekly_x = PANAS_df[['avg_weekly_PA', 'avg_weekly_NA', 'avg_weekly_N
 PANAS_short_weighted_weekly_x = PANAS_df[['weighted_avg_weekly_PA',
                        'weighted_avg_weekly_NA', 'weighted_avg_weekly_NA/PA']]
 PANAS_short_daily_x = PANAS_df[['total_PA', 'total_NA', 'total_NA/PA']]
+
 PANAS_x = PANAS_df
 PANAS_pca_x = reduced_PANAS_df[['PCA_'+str(i) for i in range(PANAS_reduced_n)]]
 PANAS_kernel_pca_x = reduced_PANAS_df[['KernelPCA_'+str(i) for i in range(PANAS_reduced_n)]]
@@ -234,88 +209,45 @@ PANAS_truncated_svd_x = reduced_PANAS_df[['TruncatedSVD_'+str(i) for i in range(
 inds = range(len(y))
 # for i in first_inds:
 #         inds.remove(i)
-ind_train, ind_test = split_data_ind(inds)
+for i in non_original_inds:
+         inds.remove(i)
+ind_train, ind_test = split_data_ind(inds, int(TEST_RATIO*len(y)))
 # ind_train = list(ind_train) + list(first_inds)
+ind_train = list(ind_train) + list(non_original_inds)
+
+print '\n dataset size:'
+print len(y)
 print '\ntrain indices:'
 print ind_train
 print '\ntest indices:'
 print ind_test
 
-# TODO: add models that are robust to outlier
+# regression, regularized versions, models that are robust to outliers
 # TODO: add hierarchical bayes
-models = ['regression', 'ridge', 'lasso', 'elasticNet', 'theil', 'ransac', 'huber']
+models = ['regression', 'ridge', 'lasso', 'elasticNet', 'huber', 'ransac', 'theil']
+model_file = open(MODEL_FILE, "w")
+model_file.close()
 for mdl in models:
-    impute(all_x, y, 'all data', mdl, ind_train, ind_test)
-    impute(pca_x, y, 'PCA', mdl, ind_train, ind_test)
-    impute(kernel_pca_x, y, 'Kernel PCA', mdl, ind_train, ind_test)
-    impute(truncated_svd_x, y, 'Truncated SVD', mdl, ind_train, ind_test)
-    impute(PANAS_short_weekly_x, y, 'PANAS short weekly', mdl, ind_train, ind_test)
-    impute(PANAS_short_weighted_weekly_x, y, 'PANAS short weighted weekly', mdl, ind_train, ind_test)
-    impute(PANAS_short_daily_x, y, 'PANAS short daily', mdl, ind_train, ind_test)
-    impute(PANAS_x, y, 'PANAS', mdl, ind_train, ind_test)
-    impute(PANAS_pca_x, y, 'PANAS PCA', mdl, ind_train, ind_test)
-    impute(PANAS_kernel_pca_x, y, 'PANAS Kenrel PCA', mdl, ind_train, ind_test)
-    impute(PANAS_truncated_svd_x, y, 'PANAS Truncated SVD', mdl, ind_train, ind_test)
+    model_file = open(MODEL_FILE, "a+")
+    impute(all_x, y, 'all data', mdl, ind_train, ind_test, model_file)
+    impute(pca_x, y, 'PCA', mdl, ind_train, ind_test, model_file)
+    impute(kernel_pca_x, y, 'Kernel PCA', mdl, ind_train, ind_test, model_file)
+    impute(truncated_svd_x, y, 'Truncated SVD', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_short_weekly_x, y, 'PANAS short weekly', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_short_weighted_weekly_x, y, 'PANAS short weighted weekly', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_short_daily_x, y, 'PANAS short daily', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_x, y, 'PANAS', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_pca_x, y, 'PANAS PCA', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_kernel_pca_x, y, 'PANAS Kernel PCA', mdl, ind_train, ind_test, model_file)
+    impute(PANAS_truncated_svd_x, y, 'PANAS Truncated SVD', mdl, ind_train, ind_test, model_file)
+    model_file.close()
+
 
 plot_prediction(BEST_X, BEST_Y, BEST_TTL, BEST_MDL_NAME, BEST_MDL, BEST_VALIDATION_RMSE, ind_train, ind_test)
 
-#
-# plt.show()
-################################### LEGACY #################################
-def legacy():
-    HAMD = pd.read_csv(data_dir+'daily_survey_HAMD.csv')
+impute_df = all_df[all_df['ID']!='M005']
+impute_df = impute_df[impute_df['day']>=0].reset_index(True)
+impute_df = impute_df.apply(fill_nan, axis=1)
+impute_df.drop(['original'], inplace=True, axis=1)
 
-    USER_N = 23
-
-
-    input_df = HAMD[HAMD['group']=='MDD']
-
-    label_binarizer = sklearn.preprocessing.LabelBinarizer()
-    label_binarizer.fit(range(USER_N))
-
-
-    def one_hot_id(series):
-        b = label_binarizer.transform([int(series['ID'][1:])])
-        series['ID_one_hot'] = b[0]
-        series['has_PANAS'] = [int(not np.isnan(series['total_PA1'])), int(not np.isnan(series['total_PA2']))]
-        return series
-
-    df = input_df[input_df['HAMD'] >= 0]
-
-    df = df.apply(one_hot_id, axis=1)
-    # df = df.dropna()
-    df = df.fillna(0)
-    df = df.reset_index(drop = True)
-
-
-
-    def create_PANAS_detailed_one_hot_missing():
-        x1 = np.array(df[['total_PA1', 'total_NA1', 'total_PA2', 'total_NA2', 'total_PA', 'total_NA', 'avg_weekly_PA', 'avg_weekly_NA', 'weighted_avg_weekly_PA', 'weighted_avg_weekly_NA']])
-        tmp_x2 = np.array(df[['ID_one_hot']])
-        x2 = np.squeeze(np.array([list(row) for row in tmp_x2]))
-        tmp_x3 = np.array(df[['has_PANAS']])
-        x3 = np.squeeze(np.array([list(row) for row in tmp_x3]))
-        x = np.hstack((x1, x2, x3))
-        return x
-
-    def create_PANAS_detailed_one_hot():
-        x1 = np.array(df[['total_PA1', 'total_NA1', 'total_PA2', 'total_NA2', 'total_PA', 'total_NA', 'avg_weekly_PA', 'avg_weekly_NA', 'weighted_avg_weekly_PA', 'weighted_avg_weekly_NA']])
-        tmp_x2 = np.array(df[['ID_one_hot']])
-        x2 = np.squeeze(np.array([list(row) for row in tmp_x2]))
-        x = np.hstack((x1, x2))
-        return x
-
-    def create_PANAS_overall_one_hot():
-        x1 = np.array(df[['total_PA', 'total_NA', 'avg_weekly_PA', 'avg_weekly_NA', 'weighted_avg_weekly_PA', 'weighted_avg_weekly_NA']])
-        tmp_x2 = np.array(df[['ID_one_hot']])
-        x2 = np.squeeze(np.array([list(row) for row in tmp_x2]))
-        x = np.hstack((x1, x2))
-        return x
-
-    def create_PANAS_overall():
-        x1 = np.array(df[['total_PA', 'total_NA', 'avg_weekly_PA', 'avg_weekly_NA', 'weighted_avg_weekly_PA', 'weighted_avg_weekly_NA']])
-        return x1
-
-    def create_PANAS_ratio():
-        x1 = np.array(df[['total_NA/PA']])
-        return x1
+impute_using_best_model(impute_df, BEST_TTL, BEST_MDL_NAME, BEST_MDL)
